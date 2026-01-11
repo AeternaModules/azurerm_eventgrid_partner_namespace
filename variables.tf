b26c8d710734a67f8d5aa0d487f26478f8be1f45
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:51:02 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "eventgrid_partner_namespaces" {
+  description = <<EOT
+Map of eventgrid_partner_namespaces, attributes below
+Required:
+    - location
+    - name
+    - partner_registration_id
+    - resource_group_name
+Optional:
+    - local_authentication_enabled
+    - partner_topic_routing_mode
+    - public_network_access
+    - tags
+    - inbound_ip_rule (block):
+        - action (optional)
+        - ip_mask (required)
+EOT
+
+  type = map(object({
+    location                     = string
+    name                         = string
+    partner_registration_id      = string
+    resource_group_name          = string
+    local_authentication_enabled = optional(bool, true)
+    partner_topic_routing_mode   = optional(string, "ChannelNameHeader")
+    public_network_access        = optional(string, "Enabled")
+    tags                         = optional(map(string))
+    inbound_ip_rule = optional(list(object({
+      action  = optional(string, "Allow")
+      ip_mask = string
+    })))
+  }))
+  validation {
+    condition = alltrue([
+      for k, v in var.eventgrid_partner_namespaces : (
+        v.inbound_ip_rule == null || (length(v.inbound_ip_rule) <= 16)
+      )
+    ])
+    error_message = "Each inbound_ip_rule list must contain at most 16 items"
+  }
+}
+
